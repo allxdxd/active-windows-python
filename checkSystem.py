86a6e97e90e393189ff1d95fc769cd2295473368
allxdxd <asdfnestor@gmail.com>
2023-02-16 18:58:12 -0500

    first commit

---
+++ checkSystem.py
@@ -0,0 +1,22 @@
+import ctypes
+import os
+
+class WrongOs(Exception):
+    def __init__(self, message):
+        self.message = message
+
+def checkAdmin():
+    if os.name == 'nt':
+
+        current_process = ctypes.windll.kernel32.GetCurrentProcess()
+
+        token = ctypes.c_void_p()
+        ctypes.windll.advapi32.OpenProcessToken(current_process, 0x20008, ctypes.byref(token))
+
+        admin_sid = ctypes.windll.shell32.IsUserAnAdmin()
+        if admin_sid:
+            return True
+        else:
+            return False
+    else:
+        raise WrongOs('Este Script es solo para Windows')

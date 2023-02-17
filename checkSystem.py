07e5fb7f996fa7d22a814696d109ea76403f1c18
allxdxd <asdfnestor@gmail.com>
2023-02-16 19:18:52 -0500

    new file:   .gitignore 	modified:   checkSystem.py 	modified:   main.py

--- checkSystem.py
+++ checkSystem.py
@@ -7,12 +7,9 @@ class WrongOs(Exception):
 
 def checkAdmin():
     if os.name == 'nt':
-
         current_process = ctypes.windll.kernel32.GetCurrentProcess()
-
         token = ctypes.c_void_p()
         ctypes.windll.advapi32.OpenProcessToken(current_process, 0x20008, ctypes.byref(token))
-
         admin_sid = ctypes.windll.shell32.IsUserAnAdmin()
         if admin_sid:
             return True
@@ -20,3 +17,11 @@ def checkAdmin():
             return False
     else:
         raise WrongOs('Este Script es solo para Windows')
+
+def checkWinDefender():
+    import subprocess
+    script_str = """
+    (Get-MpPreference).DisableRealtimeMonitoring
+    """
+    output = subprocess.check_output(["powershell", "-Command", script_str], input=None, stderr=subprocess.STDOUT)
+    return output.decode("utf-8")
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import checkSystem
 
+isAdmin = checkSystem.checkAdmin()
+print(isAdmin)
+
+realtimeMonitoring = eval(checkSystem.checkWinDefender())
+
+print(type(realtimeMonitoring))
